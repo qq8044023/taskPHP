9e34bb338f8462148b5896be8e98bab2167eb140
8044023@qq.com <8044023@qq.com>
2017-04-08 16:58:32 +0800

    udpate

--- tasks/demo/demoTask.php
+++ tasks/demo/demoTask.php
@@ -1,10 +1,12 @@
 <?php
 namespace tasks\demo;
 use core\lib\Task;
+
 /**
  * 测试任务
  */
 class demoTask extends Task{
+    
 	public function run(){
 	    $str="测试任务demoTask->run方法运行成功 \n";
 		echo $str;
