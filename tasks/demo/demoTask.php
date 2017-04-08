7d98a48d4b2307bfdc03b12d8a55e826bf5e1f2c
8044023@qq.com <8044023@qq.com>
2017-04-08 16:55:00 +0800

    demo

--- tasks/demo/demoTask.php
+++ tasks/demo/demoTask.php
@@ -1,15 +1,14 @@
 <?php
 namespace tasks\demo;
 use core\lib\Task;
-use core\lib\Config;
-use core\lib\Db;
 /**
  * 测试任务
  */
 class demoTask extends Task{
 	public function run(){
-	    $db=Db::setConfig(Config::get('DB'));
-        $res=$db->table("表名")->model()->select("id")->from("表名")->row();
-        var_dump($res);
+	    $str="测试任务demoTask->run方法运行成功 \n";
+		echo $str;
+		\core\lib\Log::input($str);
+		flush();
 	}
 }
